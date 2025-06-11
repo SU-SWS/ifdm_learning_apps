a652234c0de407d5fbd0693b30f384aa2ed31445
mdyoung3 <mdyoung3@stanford.edu>
2025-06-11 12:07:08 -0600

    Adding nojekyll

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     trailingSlash: true,
-    basePath: '/ifdm_learning_apps', // 👈 important
-    assetPrefix: '/ifdm_learning_apps', // 👈 important
+    // basePath: '/ifdm_learning_apps',
+    // assetPrefix: '/ifdm_learning_apps/',  // Add trailing slash
+    images: {
+        unoptimized: true  // Required for static export
+    }
 };
 
 export default nextConfig;
