0808f5b4d4d84d4a3a25361f708e9609cb1b90c5
Marc Young <mdyoung3@stanford.edu>
2025-06-24 12:35:09 -0600

    IFDM-33 - branch restriction (#10)
    * IFDM-33 - branch restriction
    
    * fixing name
    
    * Updated message
    
    * Moving file to correct folder.

---
+++ scripts/check-branch.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process');
+
+try {
+    const branch = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf8' }).trim();
+
+    if (branch !== '1.x') {
+        console.error(`Build blocked on all branches but 1.x`);
+        console.error('Builds are not allowed on this branch.');
+        process.exit(1);
+    }
+} catch (error) {
+    console.warn('Could not determine git branch, proceeding with build...');
+}
