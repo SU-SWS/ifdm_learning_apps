62cb55831952562b7d2cd45abaf02a6fbddbaee9
Marc Young <mdyoung3@stanford.edu>
2025-07-30 12:32:10 -0600

    Adding tailwind fonts (#18)
    * Adding tailwind fonts
    
    * Updating indentifier for opensans on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
         'digital-blue-opacity': 'rgba(0, 108, 184, 0.05)',
       },
       fontFamily: {
-        mule: ['"Mulish"', ...defaultTheme.fontFamily.sans]
+        'Open_sans': ['"Open_Sans"', ...defaultTheme.fontFamily.sans],
+        'Poppins': ['"Poppins"', ...defaultTheme.fontFamily.sans]
       }
     }
   }
