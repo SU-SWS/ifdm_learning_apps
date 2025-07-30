76273766bf50d2daddba53bc488dd94a7be807c2
Jen Breese <jbreese@stanford.edu>
2025-07-30 14:20:23 -0700

    IFDM-55: Updated default values on input fields (#21)
    * IFDM-55: Updated default values
    
    * fixup
    
    * fixup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { openSans } from "@/app/ui/fonts";
+import { poppins } from '@/app/ui/fonts';
+import "@/app/ui/globals.css";
+import { CiCalculator1 } from "react-icons/ci";
+
+
 
 const PageMoved = () => {
   return (
-      <div>
-        <h1>Page Moved</h1>
-        <p>This page has been moved.</p>
+      <div className="m-20">
+        <h1 className={`${poppins.className} text-[30px]  font-bold text-black mb-2`}>Applications</h1>
+        <ul >
+          <li className="mb-2">
+            <a
+              className="text-blue-500 hover:underline"
+              href="/interactives/investment-calculator"
+            >
+              <CiCalculator1 className="w-[2em] h-[2em] inline-block mr-2" />
+              Investment Calculator
+            </a>
+          </li>
+        </ul>
       </div>
   );
 }
