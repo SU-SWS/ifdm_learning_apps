caf6222d1d6de9ff59dae22e26ea348af82f9cdf
Ian Monroe <ian@ianmonroe.com>
2026-05-26 14:51:28 +0000

    IFDM-153: Set up a staging site

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     trailingSlash: true,
-    basePath: '',
+    basePath: process.env.NEXT_BASE_PATH || '',
     eslint: {
         // Directories to run ESLint on during builds
         dirs: ['app'],
